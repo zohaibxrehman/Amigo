77a4471bdde4486eafd0c3ab0896a758e155d469
zohaibxrehman <zohaibxrehman@gmail.com>
2020-10-28 17:46:58 -0400

    Add react snippet in views

---
+++ amigo/views/FinderPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class FinderPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default FinderPage
---
+++ amigo/views/PostPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class PostPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default PostPage
