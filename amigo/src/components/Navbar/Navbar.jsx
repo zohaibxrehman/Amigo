781ad60b98d55644b0f391ef601f51919d5f05d7
zohaibxrehman <zohaibxrehman@gmail.com>
2020-10-28 23:42:06 -0400

    Completed navbar mockup

--- amigo/src/components/Navbar/Navbar.jsx
+++ amigo/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.svg'
 
 export class Navbar extends Component {
     render() {
         return (
-            <div>
-                <p>hiii</p>
+            <div className="navbar">
+                <img className='logo' src={logo}></img>
+                <p className='logoName'>Amigo</p>
+                <a href="#">Contact</a>
+                <a href="#">About Us</a>
+                <a className="active" href="#">Posting</a>
+                <a href="#home">Home</a>
             </div>
         )
     }
