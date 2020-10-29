106f2053af34eb18f0bf27c522f76d5e12e6f423
Akshit Goyal <goyalakshit.ag@gmail.com>
2020-10-28 21:29:23 -0400

    Added motto component and images

---
+++ amigo/src/components/landingPageComponents/mottoComponent/Motto.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Motto extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            
+
+
+
+
+
+
+
+         );
+    }
+}
+ 
+export default Motto;
