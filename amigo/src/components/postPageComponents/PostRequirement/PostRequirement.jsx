e137802264c789caa2a281c9ea435558ea04e51b
Vishnu Varma <gvvarma2712@gmail.com>
2020-10-29 15:42:25 -0400

    Style Requirement Compnonent

--- amigo/src/components/postPageComponents/PostRequirement/PostRequirement.jsx
+++ amigo/src/components/postPageComponents/PostRequirement/PostRequirement.jsx
@@ -5,7 +5,14 @@ class PostRequirement extends Component {
     render() {
         return (
             <div id = "requirement">
-            
+                <h2>Requirement</h2>
+                <hr/>
+                <ul>
+                    <li>No Smoking</li>
+                    <li>Male</li>
+                    <li>Student</li>
+                    <li>Less Parties</li>
+                </ul>
             </div>
         )
     }
