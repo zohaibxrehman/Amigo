864f6cebbbe3d8d0df3f604438898d63ac9359e2
Akshit Goyal <goyalakshit.ag@gmail.com>
2020-10-28 20:16:37 -0400

    Background Image for Landing Page

---
+++ amigo/src/components/landingPageComponents/bannerComponent/Banner.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Banner.css'
+
+class Banner extends Component {
+    state = {  }
+    render() { 
+        return (
+        <div className='bgImage'>
+            {/* Add NavBar Here. */}
+            <h1 className='bannerTextHeader'>Find your next roommate!</h1>
+            <p className='bannerText'>
+                Torquatos nostros? quos dolores eos, qui dolorem ipsum per se texit, ne ferae quidem se repellere, idque instituit docere sic: omne animal, simul atque integre iudicante itaque aiunt hanc quasi involuta aperiri, altera occulta quaedam et voluptatem accusantium doloremque.
+            </p>
+        </div>
+        );
+    }
+}
+ 
+export default Banner;
--- amigo/src/views/LandingPage/LandingPage.jsx
+++ amigo/src/views/LandingPage/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import './LandingPage.css'
+import '../../components/landingPageComponents/bannerComponent/Banner'
+import Banner from '../../components/landingPageComponents/bannerComponent/Banner'
 
 export class LandingPage extends Component {
     render() {
         return (
             <div>
-                <p>Welcome to the Landing Page!</p>
+                <Banner />
             </div>
         )
     }
