2039891e6c81cc90026fa12cd3a0bbe5eb9485aa
Vishnu Varma <gvvarma2712@gmail.com>
2020-10-29 15:05:49 -0400

    Add basic requirement componenet

---
+++ amigo/src/components/postPageComponents/PostRequirement/PostRequirement.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './PostRequirement.css'
+
+class PostRequirement extends Component {
+    render() {
+        return (
+            <div id = "requirement">
+            
+            </div>
+        )
+    }
+}
+
+export default PostRequirement
