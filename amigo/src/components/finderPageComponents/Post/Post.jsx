c36ec77715e55bd3f78bc62171c7815da3560e80
zohaibxrehman <zohaibxrehman@gmail.com>
2020-10-29 14:52:06 -0400

    Added custom data for each post

--- amigo/src/components/finderPageComponents/Post/Post.jsx
+++ amigo/src/components/finderPageComponents/Post/Post.jsx
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
 import './Post.css'
-import userPhoto from '../../../assets/ProfilePhoto.jpeg'
 
 export class Post extends Component {
     render() {
+        const { userPhoto, userName, location, postTitle} = this.props.userInfo;
         return (
             <div className='post'>
                 <a href='/post'>
                     <img className='userPhoto' src={userPhoto} alt='user' />
                     <div className='postDesc'>
-                        <h3 className='userName'>Vishnu Varma, 19</h3>
-                        <small className='location'>UofT | Toronto</small>
-                        <p>Seeking 2 roomates, preferably UofT students</p>
+                        <h3 className='userName'>{userName}</h3>
+                        <small className='location'>{location}</small>
+                        <p>{postTitle}</p>
                     </div>
                 </a>
             </div>
