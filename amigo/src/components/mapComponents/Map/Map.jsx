d8d0f9df7403687183991bcbebf73a7554db62e0
zohaibxrehman <zohaibxrehman@gmail.com>
2020-11-07 21:05:19 -0500

    Add leaflet. Add switcher functionality to switch between maps and list view in finder.

---
+++ amigo/src/components/mapComponents/Map/Map.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Map extends Component {
+    render() {
+        return (
+            <div>
+                <p>helloooo</p>
+            </div>
+        )
+    }
+}
+
+export default Map
