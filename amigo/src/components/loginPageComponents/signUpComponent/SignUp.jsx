d9a9107e93305608ed2a4e71df9ed4d06fd0899a
Akshit Goyal <goyalakshit.ag@gmail.com>
2020-11-07 21:32:06 -0500

    Created the login and signup components and also fixed app.jss

---
+++ amigo/src/components/loginPageComponents/signUpComponent/SignUp.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import '../../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './SignUp.css'
+
+class SignUp extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <form>
+                <h3>Sign Up</h3>
+
+                <div className="form-group">
+                    <label>First name</label>
+                    <input type="text" className="form-control" placeholder="First Name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Last name</label>
+                    <input type="text" className="form-control" placeholder="Last Name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter your email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter a secure password" />
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                <p className="alreadyRegistered text-right">
+                    Already registered <a href="#">sign in?</a>
+                </p>
+            </form>
+
+         );
+    }
+}
+ 
+export default SignUp;
