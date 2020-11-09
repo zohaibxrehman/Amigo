da90a28253d5c1c507859e953ca4832cdf197aac
zohaibxrehman <zohaibxrehman@gmail.com>
2020-11-09 12:21:02 -0500

    Add user page view and user card component

---
+++ amigo/src/components/UserCard/UserCard.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './UserCard.css'
+
+export class UserCard extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default UserCard
