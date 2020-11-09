105e627e55eed210c5bffe5030f4cc2738895a66
zohaibxrehman <zohaibxrehman@gmail.com>
2020-11-09 12:42:45 -0500

    Add user photo in user card

--- amigo/src/components/UserCard/UserCard.jsx
+++ amigo/src/components/UserCard/UserCard.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import './UserCard.css'
+import userPhoto from '../../assets/ProfilePhoto.jpeg'
 
 export class UserCard extends Component {
     render() {
         return (
             <div className='userCardContainer'>
                 <div className='userCard'>
-
+                    <img src={userPhoto} alt="user photo" className='userCardPhoto'/>
                 </div>
             </div>
         )
