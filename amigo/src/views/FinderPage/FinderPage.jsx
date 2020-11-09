54e3a1becac712f98a471471a1a145f6d88cb1cc
zohaibxrehman <zohaibxrehman@gmail.com>
2020-11-08 20:51:05 -0500

    Add pagination component

--- amigo/src/views/FinderPage/FinderPage.jsx
+++ amigo/src/views/FinderPage/FinderPage.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './FinderPage.css'
-import { Filter, Posts, MapFinder } from '../../components/finderPageComponents/index.js'
+import { Filter, Posts, MapFinder, Pagination } from '../../components/finderPageComponents/index.js'
 
 export class FinderPage extends Component {
     constructor() {
@@ -25,8 +25,13 @@ export class FinderPage extends Component {
             <div className='finder'>
                 <Filter changeView={() => this.changeView()} />
                 <div id='finderView'>
+                    {view==='list' && 
+                    <React.Fragment>
+                        <Posts />
+                        <Pagination />
+                    </React.Fragment>
+                    }
                     {view==='map' && <MapFinder />}
-                    {view==='list' && <Posts />}
                 </div>
             </div>
         )
