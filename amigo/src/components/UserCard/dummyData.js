95ffaf3b2fd30666960e25e5509b20edd7fbaa21
zohaibxrehman <zohaibxrehman@gmail.com>
2020-11-09 14:22:27 -0500

    Changes to dummy data

--- amigo/src/components/UserCard/dummyData.js
+++ amigo/src/components/UserCard/dummyData.js
@@ -4,7 +4,9 @@ const userData = {
                     userPhoto: userPhoto,
                     userName: 'Zohaib Rehman',
                     location: 'UofT | Toronto',
-                    aboutMe: 'I am third computer science student at UofT who is passionate about astronomy and kickboxing. I love cooking and would love cook some of my traditional delicacies for my future roommate!',
+                    aboutMe: 'I am third computer science student at UofT who is passionate about astronomy and kickboxing. ' +
+                    'I love cooking and would love cook some of my traditional delicacies for my future roommate! ' +
+                    'Feel free to contact me by email or phone.',
                     contact: {
                         email: 'zohaibxrehman@gmail.com',
                         phone: '(437)-226-2963'
