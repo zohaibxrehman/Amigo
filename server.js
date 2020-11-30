38bfaebe802df7064c50c4a184acefd84bc38d7a
zohaibxrehman <zohaibxrehman@gmail.com>
2020-11-30 14:03:49 -0500

    Create server.js file

---
+++ server.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const express = require("express");
+// starting the express server
+const app = express();
+const port = process.env.PORT || 5000;
+const path = require('path')
+
+// mongoose and mongo connection
+const { mongoose } = require("./db/mongoose");
+mongoose.set('useFindAndModify', false);
+
+// to validate object IDs
+const { ObjectID } = require("mongodb");
+
+// body-parser: middleware for parsing HTTP JSON body into a usable object
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+// express-session for managing user sessions
+const session = require("express-session");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function isMongoError(error) { // checks for first error returned by promise rejection if Mongo database suddently disconnects
+    return typeof error === 'object' && error !== null && error.name === "MongoNetworkError"
+}
+
+// middleware for mongo connection error for routes that need it
+const mongoChecker = (req, res, next) => {
+    // check mongoose connection established.
+    if (mongoose.connection.readyState != 1) {
+        log('Issue with mongoose connection')
+        res.status(500).send('Internal server error')
+        return;
+    } else {
+        next()  
+    }   
+}
+
+/*** Session handling **************************************/
+// Create a session and session cookie
+app.use(
+    session({
+        secret: "our hardcoded secret",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            expires: 60000,
+            httpOnly: true
+        }
+    })
+);
+
+
+// Serve the build
+// app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.listen(port, () => {
+    log(`Listening on port ${port}...`);
+});
