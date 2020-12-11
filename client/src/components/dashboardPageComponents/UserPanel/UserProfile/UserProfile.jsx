281e7800e328294de7f59930dd62a31c62bec448
zohaibxrehman <zohaibxrehman@gmail.com>
2020-12-10 22:35:21 -0500

    Add admin icon

--- client/src/components/dashboardPageComponents/UserPanel/UserProfile/UserProfile.jsx
+++ client/src/components/dashboardPageComponents/UserPanel/UserProfile/UserProfile.jsx
@@ -1,22 +1,23 @@
 import React, { Component } from 'react'
 import './UserProfile.css'
+import adminIcon from '../../../../assets/admin-icon.png'
 
 class UserProfile extends Component {
     render() {
-        const { name, photo} = this.props;
+        const { name } = this.props;
         return (
             <div id="userProfile">
                 <ul>
                     <li>
-                        <img src={ photo } alt=""/>
+                        <img src={ adminIcon } alt=""/>
                     </li>
                     <li>
                         <ul>
                             <li> 
                                 <strong> {name.toUpperCase()} </strong>
                             </li>
-                            <li className="mainAd">
-                                (Main Adminstrator)
+                            <li>
+                                (Amigos Moderator)
                             </li>
                         </ul>
                     </li>
